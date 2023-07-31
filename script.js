ad7e31acba8a7435b8f5706638de6442def1a122
mvlopesm <marcos.medeiros@taugor.com.br>
2023-07-31 19:03:15 -0300

    implementação dos scripts em js para site dinâmico

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-const target = document.querySelector('.navbar')
-const animationClass = 'sticky'
-
-function animeScroll () {
-    const windowTop = window.pageYOffset
-    console.log(windowTop)
-    if (windowTop > 20) {
-        target.classList.add('sticky')
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
     } else {
-        target.classList.remove('sticky')
+      $(".navbar").removeClass("sticky");
     }
-}
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-window.addEventListener('scroll', function () {
-    animeScroll();
-})
+  var typed = new Typed(".typing", {
+    strings: ["Desenvolvedor Fullstack", "Web Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  var typed = new Typed(".typing-2", {
+    strings: ["Desenvolvedor Fullstack", "Web Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPauser: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
